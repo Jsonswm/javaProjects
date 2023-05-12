766f7b5fd96c66b8bb7d791f03e078f09c49c9c9
Jsonswm <2921585562@qq.com>
2023-05-12 20:38:59 +0800

    init

--- src/main/java/com/ming/logistics/utils/MD5.java
+++ src/main/java/com/ming/logistics/utils/MD5.java
@@ -47,13 +47,13 @@ public class MD5 {
         return twoCode;
     }
 
-    public static void main(String[] args) {
-        MD5 md5=new MD5();
-        md5.encode("1");
-        String content = "1";
-        String encoding = md5.encode("root");
-        System.out.println(encoding);
-
-
-    }
+//    public static void main(String[] args) {
+//        MD5 md5=new MD5();
+//        md5.encode("1");
+//        String content = "1";
+//        String encoding = md5.encode("root");
+//        System.out.println(encoding);
+//
+//
+//    }
 }
